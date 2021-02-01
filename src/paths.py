2fe0b0df21f4de3d9ee85931690c67c48bc96201
Aditya Modi <aditya.modi.in@gmail.com>
2021-02-01 19:29:06 +0530

    Commenced experiment with registration. Recorded a few videos with printed arena and warped them. Will now create a master binary maps for scoring regions, penalty regions, etc. and check the results with registration.

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,9 @@
-import os
 import inspect
+import os
 
 current_file_path = inspect.getfile(inspect.currentframe())
 project_folder_path = os.path.dirname(os.path.dirname(os.path.abspath(current_file_path)))
 src_folder_path = os.path.join(project_folder_path, 'src')
 resrc_folder_path = os.path.join(project_folder_path, 'resrc')
 data_folder_path = os.path.join(project_folder_path, 'data')
+experiments_folder_path = os.path.join(project_folder_path, 'experiments')
